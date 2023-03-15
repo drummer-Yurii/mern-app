b5e17cea53aabc0f70bf4b97c04dc8311fc1a589
Yurii Rybachok <drummer2009@ukr.net>
2023-03-15 10:26:13 +0200

    add user model

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
 
 
 /* CONFIGURATIONS */
@@ -33,8 +34,10 @@ const storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
-
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
