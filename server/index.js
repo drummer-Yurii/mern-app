50b58af69dfaf5d47961eb928af0ea7543e02309
Yurii Rybachok <drummer2009@ukr.net>
2023-03-15 12:38:46 +0200

    user routes setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 
 
@@ -42,6 +44,8 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
